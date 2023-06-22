4c35ea2c8765ab5621cb8cefa1c0d2b36e291dbe
Philip Louis Calub <philiplouis@codevidia.com>
2023-06-22 08:39:27 +0800

    Change API endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './styles/all.scss'
 import { PublicRoutes, PrivateRoutes } from './pages/Routes';
 import axios from 'axios';
 
-axios.defaults.baseURL = import.meta.env.VITE_BASE_API_URL;
+axios.defaults.baseURL = 'https://api.carehub.harayadevstudio.tech/api/';
 
 function App() {
   return(
